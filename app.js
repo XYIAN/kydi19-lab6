1b48285c0a76b061bf38b1fd8b36aed709728201
EC2 Default User <ec2-user@ip-172-31-31-17.us-east-2.compute.internal>
2020-04-19 03:48:55 +0000

    fix

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.listen(process.env.PORT, process.env.IP , function()//using local host port 
        console.log("Opening with KEY::ID" + (i*34) + " ACCESS");
        console.log("Opening with KEY::ID" + (i*346)+ 0 + " accessACCESS");
     }
-    console.log("Welcome Mr. Dilbeck, currently retreiving IP ::::");
+    console.log("Welcome , currently retreiving IP ::::");
     console.log("Express Server is now Running...");
 });
 
